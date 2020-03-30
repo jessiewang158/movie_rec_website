252bcb0fa99cf0a6ece98a1e4b38a7a5c2c6cd2a
Jessie Wang <45979098+jessiewang158@users.noreply.github.com>
2020-03-30 14:44:26 -0600

    Update example2.py

--- example2.py
+++ example2.py
@@ -17,8 +17,8 @@ def make_rec (newmovieId=[2,3],newrating=[4,5]):
     result=CL.recalculate_user(newmovieId,newrating)
     #result=CL.recalculate_user([m[0] for m in args.m],[r[0] for r in args.r])
     print("recmovie",result)
-    p_at_k, m_at_k = CL.evaloutput(10)
-    print(p_at_k, m_at_k)
+    #p_at_k, m_at_k = CL.evaloutput(10)
+    #print(p_at_k, m_at_k)
 
     #similar = CL.most_similar_items(smovieid, n_similar=10)
     #print('similarmovie',similar)
