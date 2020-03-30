23bf5a6e51250b63b37e01ea671400268809b494
Jessie Wang <45979098+jessiewang158@users.noreply.github.com>
2020-03-30 14:46:01 -0600

    Update example_lessdata.py

--- example_lessdata.py
+++ example_lessdata.py
@@ -6,5 +6,5 @@ def make_rec (newmovieId=[2,3],newrating=[4,5]):
     result = CL.recalculate_user(newmovieId, newrating)
     print("recmovie", result)
 
-    p_at_k, m_at_k = CL.evaloutput(10)
-    print(p_at_k, m_at_k)
+    #p_at_k, m_at_k = CL.evaloutput(10)
+    #print(p_at_k, m_at_k)
